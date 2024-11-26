9b0b522c483b188e10b1e4404dc3453a26c83940
Siva0717 <sivananthansivananthan533@gmail.com>
2024-11-26 22:50:09 +0530

    Add files via upload

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { TriggerComponent } from './trigger/trigger.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+    
+path:'register-form',
+component: RegisterComponent,
+   } ,
+   {
+    path:'login-form',
+    component:LoginComponent,
+   },
+   {
+path:'',
+component:AppComponent,
+   },
+   {
+path:'trigger',
+component:TriggerComponent,
+   }
+   
+];
+
