fbcae211e2fe1d7b51e765edafc34ff94aa7d285
RITHIKASHREEBALAKRISHNAN <email@rithikabala6@gmail.com>
2024-11-29 21:24:50 +0530

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,37 @@
+import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ServiceComponent } from './service/service.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+export const routes: Routes = [
+    {
+        path:'header',
+        component:HeaderComponent,
+        children:[
+            {
+                path:'home',
+                component:HomeComponent,
+            },
+            {
+                path:'service',
+                component:ServiceComponent,
+
+            },
+            
+            {
+                path:'about',
+                component:AboutComponent
+            },
+            {
+                path:'contact',
+                component:ContactComponent
+            }
+        ]
+
+    }
+    
+];
+
